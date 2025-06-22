855ca465c42eb3015bca3d3e58f29cabfb2174a1
Hudater <Hudater@users.noreply.github.com>
2025-06-22 16:23:49 +0000

    deploy: dab0fd37ec5e7c76c3fff1305a736989fd42a6d9

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1750609428',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };

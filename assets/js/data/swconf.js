5b88756fb7fe8e7925ce85a0cfd0d5126807d4e2
Hudater <Hudater@users.noreply.github.com>
2026-01-01 22:03:19 +0000

    deploy: cda5da8edd915c47ec1db09b60842f8f494bfb95

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1767284458',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1767304999',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ 'https://hudater.goatcounter.com/counter/' ] }, purge: false };
